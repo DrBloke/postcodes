9230a60525652b70d861d118aceb3a0f5850b319
Sam <sam@samtracey.co.uk>
2018-05-30 14:04:33 +0100

    Working except for tests and autocomplete.

--- src/Main.elm
+++ src/Main.elm
@@ -14,6 +14,7 @@ import Json.Decode exposing (Decoder, string)
 import Json.Decode.Pipeline exposing (decode, required)
 import Task
 import Platform.Cmd exposing (batch)
+import Parser exposing (..)
 
 
 main : Program Never Model Msg
@@ -35,7 +36,9 @@ initialModel route =
     { route = route
     , history = []
     , locationInfo = NotAsked
+    , nearestInfo = NotAsked
     , postcode = "CB3 0FA"
+    , validPostcode = parsePostcode "CB3 0FA"
     }
 
 
@@ -57,49 +60,87 @@ type Msg
     = ChangeLocation String
     | OnLocationChange Navigation.Location
     | LocationInfoResponse (WebData LocationInfo)
+    | NearestInfoResponse (WebData NearestInfo)
     | ChangePostcode Postcode
     | GetDataFromWeb
+    | GetNearestInfoFromWeb
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ChangeLocation path ->
-            ( { model
-                | history = path :: model.history
-              }
-            , Navigation.newUrl path
-            )
+            if path == "" || path == "/" then
+                ( { model | route = HomeRoute }, Cmd.none )
+            else
+                let
+                    validatedPostcode =
+                        parsePostcode path
+                in
+                    case validatedPostcode of
+                        Ok postcode ->
+                            ( { model
+                                | history = postcode :: model.history
+                              }
+                            , Navigation.newUrl postcode
+                            )
+
+                        Err e ->
+                            ( { model | route = InvalidRoute e }, Cmd.none )
 
         OnLocationChange location ->
             let
                 newRoute =
                     parseLocation location
+
+                --url will often have %20 in and this needs to be changed back to space
+                decodedPostcodeUrl =
+                    Maybe.withDefault "" <| decodeUri <| Maybe.withDefault "" (parsePath UrlParser.string location)
             in
                 { model
                     | route = newRoute
-                    , postcode = Maybe.withDefault "" <| decodeUri <| Maybe.withDefault "" (parsePath UrlParser.string location)
+                    , postcode = decodedPostcodeUrl
+                    , validPostcode = parsePostcode decodedPostcodeUrl
                 }
                     |> update GetDataFromWeb
 
         --web response
         LocationInfoResponse response ->
-            ( { model | locationInfo = response }, Cmd.none )
+            { model | locationInfo = response }
+                |> update GetNearestInfoFromWeb
+
+        NearestInfoResponse response ->
+            ( { model | nearestInfo = response }, Cmd.none )
 
         ChangePostcode postcode ->
             ( { model | postcode = postcode }, Cmd.none )
 
         GetDataFromWeb ->
-            ( model
-            , getLocationInfo model.postcode
-            )
+            case model.validPostcode of
+                Ok postcode ->
+                    ( model
+                    , getLocationInfo model.postcode
+                    )
+
+                Err e ->
+                    ( model, Cmd.none )
+
+        GetNearestInfoFromWeb ->
+            case model.validPostcode of
+                Ok postcode ->
+                    ( model
+                    , getNearestInfo model.postcode
+                    )
+
+                Err e ->
+                    ( model, Cmd.none )
 
 
 
 --web request
 
 
-getLocationInfo : Postcode -> Cmd Msg
+getLocationInfo : String -> Cmd Msg
 getLocationInfo postcode =
     Http.get
         ("http://api.postcodes.io/postcodes/"
@@ -110,6 +151,18 @@ getLocationInfo postcode =
         |> Cmd.map LocationInfoResponse
 
 
+getNearestInfo : String -> Cmd Msg
+getNearestInfo postcode =
+    Http.get
+        ("http://api.postcodes.io/postcodes/"
+            ++ postcode
+            ++ "/nearest"
+        )
+        decodeNearestInfo
+        |> RemoteData.sendRequest
+        |> Cmd.map NearestInfoResponse
+
+
 
 -- VIEWS
 
@@ -118,48 +171,85 @@ view : Model -> Html Msg
 view model =
     div []
         [ div []
-            [ input [ type_ "text", placeholder "Postcode", onInput ChangePostcode, value model.postcode ] []
+            [ a [ href homePath, onLinkClick (ChangeLocation homePath) ] [ text "Home" ]
+            , br [] []
+            , br [] []
+            , input [ type_ "text", placeholder "Postcode", onInput ChangePostcode, value model.postcode ] []
             , button [ onClick <| ChangeLocation model.postcode ] [ text "Get info" ]
+            , br [] []
+            , br [] []
             ]
-        , div []
-            [ page model.route model.locationInfo
-            , nav model.postcode
-            , ul [] (history model.history) --see Router module
-            ]
+        , page model
         ]
 
 
-page : Route -> WebData LocationInfo -> Html Msg
-page route locationInfo =
-    case route of
+page : Model -> Html Msg
+page model =
+    case model.route of
         HomeRoute ->
-            text "Home"
+            div []
+                [ text "Enter a postcode above"
+                , viewHistory model.history
+                ]
 
         PostcodeRoute postcode ->
-            case locationInfo of
-                NotAsked ->
-                    text "Initialising."
+            let
+                combinedResponse =
+                    RemoteData.map2 (,) model.locationInfo model.nearestInfo
+            in
+                case combinedResponse of
+                    NotAsked ->
+                        text "Initialising."
 
-                Loading ->
-                    text "Loading."
+                    Loading ->
+                        div [] [ text "Loading." ]
 
-                Failure err ->
-                    text ("Error: " ++ toString err)
+                    Failure err ->
+                        viewServerError err
 
-                Success locationInfo ->
-                    viewLocationInfo locationInfo
+                    Success combinedInfo ->
+                        viewInfo combinedInfo
+
+        InvalidRoute e ->
+            case (List.head e.context) of
+                Just context ->
+                    text <| "The " ++ context.description ++ " of your postcode is incorrect."
+
+                Nothing ->
+                    text "There is a problem with your postcode."
 
         NotFoundRoute ->
             text "Not Found"
 
 
-viewLocationInfo : LocationInfo -> Html msg
-viewLocationInfo locationInfo =
+viewInfo : ( LocationInfo, NearestInfo ) -> Html msg
+viewInfo ( locationInfo, nearestInfo ) =
     div []
         [ text locationInfo.result.country
         , br [] []
         , text <| Maybe.withDefault "No region" locationInfo.result.region
+        , br [] []
+        , text <| "Nearest postcodes: " ++ String.join "; " (List.drop 1 <| List.map (\a -> a.postcode) nearestInfo.result)
         ]
+
+
+viewServerError : Http.Error -> Html msg
+viewServerError err =
+    case err of
+        Http.BadStatus response ->
+            if response.status.code == 404 then
+                div []
+                    [ text ("Although valid, that postcode does not seem to exist.") ]
+            else
+                div []
+                    [ text
+                        ("There was a problem. The server said: "
+                            ++ response.status.message
+                        )
+                    ]
+
+        _ ->
+            div [] [ text ("There was a problem contacting the server.") ]
 
 
 
@@ -170,9 +260,6 @@ nav : String -> Html Msg
 nav postcode =
     div []
         [ a [ href homePath, onLinkClick (ChangeLocation homePath) ] [ text "Home" ]
-        , text " "
-        , a [ href <| postcodePath postcode, onLinkClick (ChangeLocation <| postcodePath postcode) ] [ text "Postcode" ]
-        , text (" ")
         ]
 
 
@@ -191,9 +278,9 @@ onLinkClick message =
 --save history of routes visited (use in autofill)
 
 
-history : List String -> List (Html Msg)
-history history =
-    List.map historyLinks history
+viewHistory : List String -> Html Msg
+viewHistory history =
+    ul [] (List.map historyLinks history)
 
 
 historyLinks : String -> Html Msg
--- src/Types.elm
+++ src/Types.elm
@@ -3,6 +3,9 @@ module Types exposing (..)
 import RemoteData exposing (WebData, RemoteData(..))
 import Json.Decode exposing (Decoder, string)
 import Json.Decode.Pipeline exposing (decode, required)
+import Parser exposing (..)
+import Char
+import Result
 
 
 --model
@@ -12,12 +15,67 @@ type alias Model =
     { route : Route
     , history : List String
     , locationInfo : WebData LocationInfo
-    , postcode : String
+    , nearestInfo : WebData NearestInfo
+    , postcode : Postcode
+    , validPostcode : Result Error Postcode
     }
 
 
 type alias Postcode =
     String
+
+
+parsePostcode : String -> Result Error Postcode
+parsePostcode postcode =
+    let
+        pcFirst =
+            String.toUpper <| String.trim <| String.dropRight 3 <| String.trim postcode
+
+        pcSecond =
+            String.toUpper <| String.right 3 <| String.trim postcode
+
+        parsedFirst =
+            Parser.run postcodeFirst pcFirst
+
+        parsedSecond =
+            Parser.run postcodeSecond pcSecond
+    in
+        Result.map2 (\b a -> a ++ " " ++ b) parsedSecond parsedFirst
+
+
+postcodeFirst : Parser String
+postcodeFirst =
+    inContext "first part" <|
+        source <|
+            ignore (Exactly 1) Char.isUpper
+                |. oneOf
+                    [ ignore (Exactly 1) Char.isUpper
+                    , succeed ()
+                    ]
+                |. ignore (Exactly 1) Char.isDigit
+                |. oneOf
+                    [ (ignore (Exactly 1) Char.isDigit)
+                    , succeed ()
+                    ]
+                |. oneOf
+                    [ ignore (Exactly 1) Char.isUpper
+                    , succeed ()
+                    ]
+                |. spaces
+                |. end
+
+
+postcodeSecond : Parser String
+postcodeSecond =
+    inContext "second part" <|
+        source <|
+            ignore (Exactly 1) Char.isDigit
+                |. ignore (Exactly 2) Char.isUpper
+
+
+spaces : Parser ()
+spaces =
+    ignore zeroOrMore (\c -> c == ' ')
 
 
 
@@ -27,6 +85,7 @@ type alias Postcode =
 type Route
     = HomeRoute
     | PostcodeRoute String
+    | InvalidRoute Error
     | NotFoundRoute
 
 
@@ -47,6 +106,17 @@ type alias LocationInfoResult =
     }
 
 
+type alias NearestInfo =
+    { status : Int
+    , result : List NearestInfoResult
+    }
+
+
+type alias NearestInfoResult =
+    { postcode : String
+    }
+
+
 
 --decoders
 
@@ -64,3 +134,20 @@ decodeLocationInfoResult =
         |> Json.Decode.Pipeline.required "postcode" (Json.Decode.string)
         |> Json.Decode.Pipeline.required "country" (Json.Decode.string)
         |> Json.Decode.Pipeline.required "region" (Json.Decode.nullable string)
+
+
+
+---nearest
+
+
+decodeNearestInfo : Json.Decode.Decoder NearestInfo
+decodeNearestInfo =
+    Json.Decode.Pipeline.decode NearestInfo
+        |> Json.Decode.Pipeline.required "status" (Json.Decode.int)
+        |> Json.Decode.Pipeline.required "result" (Json.Decode.list decodeNearestInfoResult)
+
+
+decodeNearestInfoResult : Json.Decode.Decoder NearestInfoResult
+decodeNearestInfoResult =
+    Json.Decode.Pipeline.decode NearestInfoResult
+        |> Json.Decode.Pipeline.required "postcode" (Json.Decode.string)
